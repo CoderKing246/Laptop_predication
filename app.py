f71670e71f70ad563bda114744cddb59c850c1c6
CoderKing246 <mehrahitesh2006@gmail.com>
2024-10-27 16:21:08 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,80 @@
+import streamlit as st  
+import pickle 
+import numpy as np 
+
+# Load the trained model
+with open('pipe.pkl', 'rb') as f:
+    pipe = pickle.load(f)
+
+with open('df.pkl', 'rb') as f:
+    df = pickle.load(f)
+
+st.title('Laptop Price Predictor')
+
+# Brand 
+company = st.selectbox('Brand', df['Company'].unique())
+
+# Type
+type = st.selectbox('Type', df['TypeName'].unique())
+
+# RAM
+ram = st.selectbox('RAM (in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
+
+# Weight
+weight = st.number_input('Weight of the laptop')
+
+# Touchscreen
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
+
+# IPS
+ips = st.selectbox('IPS', ['No', 'Yes'])
+
+# Screen size
+screen_size = st.number_input('Screen Size')
+
+# Resolution
+resolution = st.selectbox('Screen Resolution', [
+    '1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800',
+    '2880x1800', '2560x1600', '2560x1440', '2304x1440'
+])
+
+# CPU
+cpu = st.selectbox('CPU', df['Cpu Brand'].unique())
+
+# HDD
+hdd = st.selectbox('HDD (in GB)', [0, 128, 256, 512, 1024, 2048])
+
+# SSD
+ssd = st.selectbox('SSD (in GB)', [0, 8, 128, 256, 512, 1024])
+
+# GPU
+gpu = st.selectbox('GPU', df['Gpu Brand'].unique())
+
+# OS
+os = st.selectbox('OS', df['os'].unique())
+
+if st.button('Predict Price'):
+    # Prepare query
+    ppi = None
+    if touchscreen == 'Yes':
+        touchscreen = 1
+    else:
+        touchscreen = 0
+
+    if ips == 'Yes':
+        ips = 1
+    else:
+        ips = 0
+
+    # Calculate Pixels Per Inch (PPI)
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+    ppi = ((X_res**2) + (Y_res**2))**0.5 / screen_size
+
+    # Arrange query in the correct shape for prediction
+    query = np.array([company, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
+    query = query.reshape(1, -1)  # Reshape to match model input shape
+
+    # Predict and display the result
+    predicted_price = np.exp(pipe.predict(query)[0])  # Use exponential if the model predicts log prices
+    st.title("The predicted price of this configuration is $" + str(int(predicted_price)))
